fe94a44ef8bc47849f154819f617a22667e9b1a2
shakthi <shakthi@edumerge.com>
2025-07-05 21:14:29 +0530

    Add Tailwind config

---
+++ tailwind.config.ts
@@ -0,0 +1,18 @@
+// tailwind.config.js
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#027D6A', // 👈 your custom green color
+      },
+    },
+  },
+  plugins: [],
+};
